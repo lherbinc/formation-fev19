e15e4507c75631c80d297241fd9fe4e5ddb45c72
Laetitia Herbin-Collot <laetic@aol.com>
2019-02-13 10:52:45 +0100

    Utilisation ServeIndex

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const serveIndex = require('serve_index');
+const app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+//app.get('/', function (req, res) {
+//  res.send('Hello World!');
+//});
+
+// middleware : si l'url correspond à un fichier c'est tout. si ne correspond pas au fichier, passe au middleware suivant
+app.use(express.static('.')); 
+
+// middleware qui sert à afficher joliment les répertoires
+app.use(serveIndex('public/ftp', {'icons': true}));
+
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+  console.log('Example app listening on port 3000!');
+});
+
