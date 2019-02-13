c6b639b45113598ae42c23cc0a2436137be74529
Laetitia Herbin-Collot <laetic@aol.com>
2019-02-13 11:09:55 +0100

    accès www

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const serveIndex = require('serve-index');
 const app = express();
+const www = 'www';
 
 //app.get('/', function (req, res) {
 //  res.send('Hello World!');
 //});
 
 // middleware : si l'url correspond à un fichier c'est tout. si ne correspond pas au fichier, passe au middleware suivant
-app.use(express.static('..')); 
+app.use(express.static(www)); 
 
 // middleware qui sert à afficher joliment les répertoires
-app.use(serveIndex('..', {'icons': true}));
+app.use(serveIndex(www, {'icons': true}));
 
 // autre façon d'écrire un middleware
 app.use((req, res, next) => {
