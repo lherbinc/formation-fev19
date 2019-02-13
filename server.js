b0aed7f99fba56b7bf30a2fc227beec5f6fd7025
Laetitia Herbin-Collot <laetic@aol.com>
2019-02-13 12:01:54 +0100

    typescript

--- server.js
+++ server.js
@@ -15,5 +15,5 @@ app.use(function (req, res, next) {
     next();
 });
 app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+    console.log('Example app listening test1 on port 3000!');
 });
