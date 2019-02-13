c5c24a8736240e670520b146cd1a5bbdc4007165
Laetitia Herbin-Collot <laetic@aol.com>
2019-02-13 11:50:32 +0100

    transpile

--- server_versionES6.js
+++ server_versionES6.js
@@ -1,19 +1,26 @@
-var express = require('express');
-var serveIndex = require('serve-index');
-var app = express();
-var www = 'www';
+const express = require('express');
+const serveIndex = require('serve-index');
+const app = express();
+const www = 'www';
+
 //app.get('/', function (req, res) {
 //  res.send('Hello World!');
 //});
+
 // middleware : si l'url correspond à un fichier c'est tout. si ne correspond pas au fichier, passe au middleware suivant
-app.use(express.static(www));
+app.use(express.static(www)); 
+
 // middleware qui sert à afficher joliment les répertoires
-app.use(serveIndex(www, { 'icons': true }));
+app.use(serveIndex(www, {'icons': true}));
+
 // autre façon d'écrire un middleware
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
     console.log('url not found', req.url);
     next();
 });
+
+
 app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+  console.log('Example app listening on port 3000!');
 });
+
